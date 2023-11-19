795a488e4a1a9250ad03329ca358c4eda38041d1
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2023-11-19 15:48:55 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const whereToSwap = [
+  {name: 'Avnu', chains: 'Starknet', mingoRating: 4, url: 'https://app.avnu.fi/en'},
+  {name: 'Ekubo', chains: 'Starknet', mingoRating: 4, url: 'https://app.ekubo.org/'},
+  {name: 'Ambient Finance', chains: ['Scroll', 'Ethereum'], mingoRating: 4, banklessLikelyhood: 'Medium', url: 'https://ambient.finance/'},
+  {name: 'Odos', chains: ['Ethereum', 'Optimism', 'BNB Chain', 'Polygon', 'zkSync Era', 'Base', 'Arbitrum', 'Avalanche'], mingoRating: 3, url: 'https://odos.xyz/'},
+  {name: 'SyncSwap', chains: ['Ethereum','zkSync Era', 'Linea', 'Scroll'], mingoRating: 2, banklessLikelyhood: 'High', url: 'https://syncswap.xyz/'},
+  {name: 'JediSwap', chains: 'Starknet', mingoRating: 2, banklessLikelyhood: 'Medium', url: 'https://www.jediswap.xyz/'},
+  {name: 'DefiLlama', chains: 'All other chains', url: 'https://swap.defillama.com/'}
+];
+
 function App() {
+  const [selectedChain, setSelectedChain] = useState('Starknet');
+  const [swapResults, setSwapResults] = useState([]);
+
+  const handleFindSwap = () => {
+    const chainMatches = whereToSwap.filter((dex) => {
+      if (Array.isArray(dex.chains)) {
+        return dex.chains.includes(selectedChain);
+      } else {
+        return dex.chains === selectedChain;
+      }
+    });
+
+    setSwapResults(chainMatches);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Swap Finder</h1>
+      <label htmlFor='chainDropdown'>Select Chain:</label>
+      <select
+        id="chainDropdown"
+        onChange={(e) => setSelectedChain(e.target.value)}
+        value={selectedChain}
+      >
+        {Array.from(new Set(whereToSwap.flatMap((swap) => swap.chains)))
+          .filter((chain) => chain !== undefined)
+          .map((chain) => (
+            <option key={chain} value={chain}>
+              {chain}
+            </option>
+          ))}
+      </select>
+      <button onClick={handleFindSwap}>Find swap</button>
+      
+      <div>
+        {swapResults.length > 0 ? (
+          <ul>
+            {swapResults.map((result) => (
+              <li key={result.name}>
+                <a href={result.url}>{result.name}</a>{result.mingoRating && (<span> - Mingo Rating: {result.mingoRating} </span>)}
+                {result.banklessLikelyhood && (
+                  <span> - Bankless likelyhood: {result.banklessLikelyhood}</span>
+                )}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Select chain and click Find Swap.</p>
+        )}
+      </div>
     </div>
   );
 }
